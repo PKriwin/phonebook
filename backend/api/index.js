ecba18079f06dabd180b4cf4d8ed8113b55d5134
Paul_Kriwin <paul.kriwin@student.uclouvain.be>
2018-10-10 16:15:30 +0200

    refactor, dbAdaptor options are now passed as param

--- backend/api/index.js
+++ backend/api/index.js
@@ -5,9 +5,19 @@ const routes    = require(path.join(__dirname, 'routes'))
 const dbAdapter = require(path.join(__dirname, 'database', 'sequelizeDbAdapter'))
 const apiServer = require(path.join(__dirname, 'server'))
 
+const options = {
+  DB_NAME: 'test',
+  DB_USERNAME: 'user',
+  DB_PASSWORD: 'pswd',
+  DB_HOST: 'localhost',
+  DB_DIALECT: 'sqlite',
+  DB_STORAGE: './test.db',
+  DB_LOGGING: console.log
+}
+
 async function main () {
 
-  await apiServer.init(dbAdapter, routes)
+  await apiServer.init(await dbAdapter.init(options), routes)
   await apiServer.start()
 }
 
